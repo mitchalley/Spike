8eb219be9fa9a0b8ea96b3a547c11afef7503ebf
Mitch <mgalley@wisc.edu>
2021-02-16 19:29:12 -0600

    Initial Commit

---
+++ Shared/Spike_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spike_ExerciseApp.swift
+//  Shared
+//
+//  Created by Mitch Alley on 2/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Spike_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
